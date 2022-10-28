6b80883188d42ca5419466e269be7158ac8654c4
Ml06py <liaghimohamad69@gmail.com>
2022-10-28 10:36:56 +0330

    Update profile permission fixed

--- v1/accounts/views.py
+++ v1/accounts/views.py
@@ -1,4 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 from django.shortcuts import get_object_or_404
 
 from .viewsets import ListRetrieveUpdateViewSet
@@ -18,13 +21,15 @@ class ProfileViewSet(ListRetrieveUpdateViewSet):
 
         return Profile.objects.filter(user=self.request.user)
 
+
     def get_object(self):
         if self.request.user.role in ["ad", 'su', 't']:
             return get_object_or_404(Profile, id=self.kwargs["pk"])
 
         return get_object_or_404(Profile, id=self.kwargs["pk"], user=self.request.user)
     
-    
+
+
     def get_serializer_class(self):
         # return appropriate serializer
 
@@ -33,4 +38,14 @@ class ProfileViewSet(ListRetrieveUpdateViewSet):
 
         elif self.action in ["retrieve", "update", "partial_update"]:
             return ProfileDetailSerializer
+
+
+    def update(self, request, *args, **kwargs):
+        '''Dont let others to update a users profile'''
+
+        if self.request.user == self.get_object().user:
+            return super().update(request, *args, **kwargs)
+
+        return Response("You dont have permission to update this profile.", status=status.HTTP_403_FORBIDDEN)
+            
 
