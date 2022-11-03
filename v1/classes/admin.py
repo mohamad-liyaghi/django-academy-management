ca393da830d28941b0e58c9641a9bb8e827179b4
Ml06py <liaghimohamad69@gmail.com>
2022-11-03 11:13:37 +0330

    Course model added

--- v1/classes/admin.py
+++ v1/classes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from classes.models import Course
 
-# Register your models here.
+
+@admin.register(Course)
+class Course(admin.ModelAdmin):
+    list_display = ["title", 'price', 'teacher', 'published']
--- v1/classes/models.py
+++ v1/classes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from accounts.models import User
 
-# Create your models here.
+class Course(models.Model):
+
+    class Difficulty(models.TextChoices):
+        '''Difficulty status'''
+
+        BEGINNER = ("b", "Beginner")
+        INTERMEDIATE = ("i", "Intermediate")
+        ADVANCE = ("a", "Advance")
+
+
+    title = models.CharField(max_length=220)
+    description = models.TextField()
+
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="courses")
+
+    price = models.IntegerField(default=0, validators=[
+            MaxValueValidator(5000),
+            MinValueValidator(10)
+        ])
+
+    published = models.BooleanField(default=False)
+
+    difficulty = models.CharField(max_length=1, choices=Difficulty.choices, 
+                default=Difficulty.BEGINNER)
+
+    time = models.FloatField(default=0, validators=[
+            MinValueValidator(0.0)
+        ])
+
+    token = models.CharField(unique=True, max_length=15)
+    
+    def __str__(self) -> str:
+        return self.title
