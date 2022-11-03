7b1792e404c83a3b099e0f8ad61caff283e195e9
Ml06py <liaghimohamad69@gmail.com>
2022-11-03 11:45:17 +0330

    Course ViewSet added

--- v1/classes/views.py
+++ v1/classes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from classes.models import Course
+
+
+class CourseViewSet(ModelViewSet):
+    '''A viewset for Course stuff such as `add`, `list`, `delete`, and so on'''
+
+    queryset = Course.objects.all()
