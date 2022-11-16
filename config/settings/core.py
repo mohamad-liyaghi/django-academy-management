81cff2f89a6bb1dbdf4b93d0f3baba6bfc0c45af
Ml06py <liaghimohamad69@gmail.com>
2022-11-16 15:46:18 +0330

    Postgresql db added

--- config/settings/core.py
+++ config/settings/core.py
@@ -54,12 +54,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,14 @@ from .core import *
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
+    }
+}
