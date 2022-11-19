b77acfb645ac42118e1423c29e60ef11703986c6
Ml06py <liaghimohamad69@gmail.com>
2022-11-19 13:22:53 +0330

    Djoser extra urls removed

--- v1/accounts/urls.py
+++ v1/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
+from djoser.views import UserViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
 from . import views
 
 app_name = 'v1'
@@ -9,10 +11,11 @@ router = routers.SimpleRouter()
 router.register('profile', views.ProfileViewSet)
 router.register('request', views.RequestViewSet, basename="request")
 
+register_user = UserViewSet.as_view({"post" : "create"})
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('register/', register_user, name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
     path('money/', views.get_money, name='get-money')
 ]
 
