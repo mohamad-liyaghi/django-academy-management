8aa01a85e40038c27fcb91e92c7ddf36caef628a
Ml06py <liaghimohamad69@gmail.com>
2022-11-20 13:58:33 +0330

    Gunicorn added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Apply database migrations and run"
-python manage.py makemigrations && python manage.py migrate && python manage.py runserver --insecure 0.0.0.0:8000
+python manage.py makemigrations && python manage.py migrate && gunicorn config.wsgi --bind 0.0.0.0:8000
